dd22063d9a788b3ced1b4f14e52982349d42ad07
zac <suoxd123@126.com>
2016-07-11 23:55:32 +0800

    创建类文件

---
+++ JYZN/Core/Communicate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    /// <summary>
+    /// 通信类
+    /// </summary>
+    public class Communicate
+    {
+    }
+}
