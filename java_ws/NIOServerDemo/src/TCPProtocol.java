2143d5f5f6ff704db2ce1a4c92c95314a8eb3adc
woriklj <lijiang_87@126.com>
2016-07-13 13:58:11 +0800

    测试删除

--- java_ws/NIOServerDemo/src/TCPProtocol.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.nio.channels.SelectionKey;
-
-public interface TCPProtocol {
-	void handleAccept(SelectionKey key) throws IOException;
-	void handleRead(SelectionKey key) throws IOException;
-	void handleWrite(SelectionKey key) throws IOException;
-}
