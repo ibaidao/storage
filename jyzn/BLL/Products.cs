090957688595ce66d5083c9edaa549491a86a5b3
zac <suoxd123@126.com>
2016-07-17 18:43:00 +0800

    订单

--- jyzn/BLL/Products.cs
+++ jyzn/BLL/Products.cs
@@ -13,11 +13,30 @@ namespace BLL
         /// 为拣货员分配新订单
         /// </summary>
         /// <param name="staffID">拣货员ID</param>
-        /// <returns>订单ID</returns>
+        /// <returns>新订单ID</returns>
         public int GetNewOrder(int staffID)
         {
-            RealOrders order =DbEntity.DRealOrders.First();
+            RealOrders order =DbEntity.DRealOrders.GetSingleEntity(" Status == 0 ",null);
+
             return order.ID;
+        }
+
+        /// <summary>
+        /// 为拣货员初始化订单
+        /// </summary>
+        /// <param name="staffID">拣货员ID</param>
+        /// <returns>新订单ID列表</returns>
+        public List<int> GetInitialOrder(int staffID)
+        {
+            int recordCount = 0;
+            List<int> orderIds = new List<int>();
+            List<RealOrders> orderList = DbEntity.DRealOrders.GetEntityList(" Status == 0 ", null, 1, 10,out recordCount);
+            foreach (RealOrders order in orderList)
+            {
+                orderIds.Add(order.ID);
+            }
+
+            return orderIds;
         }
     }
 }
