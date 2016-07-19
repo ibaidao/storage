f3ce3ee7a057d124ff7f6486a4d3fbf191797e1e
zac <suoxd123@126.com>
2016-07-19 10:37:41 +0800

    选货架用第二方案，第一方案做一半先备份

--- jyzn/Models/Logic/Choice.cs
+++ jyzn/Models/Logic/Choice.cs
@@ -89,31 +89,77 @@ namespace Models.Logic
             sqlParam.Add("SkuIds",strSkuId.Remove(strSkuId.Length - 1));
             List<Products> productList = DbEntity.DProducts.GetEntityList(" SkuID IN @(SkuIds) ", sqlParam);
             if (productList == null) return null;
+
+            Dictionary<int, Dictionary<int, int>> skuShelf = ClassifyShelfBySkuId(productList);
+            foreach (KeyValuePair<int, Dictionary<int, int>> shelf in skuShelf)
+            {
+
+            }
             
-            Dictionary<int,Dictionary<int,int>> productShelf = new Dictionary<int, Dictionary<int,int>>();
+        }
+
+        /// <summary>
+        /// 根据商品SKU ID分类商品货架
+        /// </summary>
+        /// <param name="productList"></param>
+        /// <returns></returns>
+        private Dictionary<int, Dictionary<int, int>> ClassifyShelfBySkuId(List<Products> productList)
+        {
+            Dictionary<int, Dictionary<int, int>> productShelf = new Dictionary<int, Dictionary<int, int>>();
             foreach (Products product in productList)
             {
-                if (!productShelf.ContainsKey(product.ID))
+                if (!productShelf.ContainsKey(product.SkuID))
                 {//新商品
-                    Dictionary<int,int> shelf= new Dictionary<int,int>();
-                    shelf.Add(product.ShelfID,product.Count);
-                    productShelf.Add(product.ID, shelf);
+                    Dictionary<int, int> shelf = new Dictionary<int, int>();
+                    shelf.Add(product.ShelfID, product.Count);
+                    productShelf.Add(product.SkuID, shelf);
                 }
-                else if (!productShelf[product.ID].ContainsKey(product.ShelfID))
+                else if (!productShelf[product.SkuID].ContainsKey(product.ShelfID))
                 {//新货架
-                    productShelf[product.ID].Add(product.ShelfID, product.Count);
+                    productShelf[product.SkuID].Add(product.ShelfID, product.Count);
                 }
                 else
                 {//已有货架
-                    productShelf[product.ID][product.ShelfID] += product.Count;
+                    productShelf[product.SkuID][product.ShelfID] += product.Count;
                 }
             }
             return productShelf;
         }
 
-        private Dictionary<int, List<List<int>>> ExplainProductShelfs(Dictionary<int, Dictionary<int, int>> productShelf)
+        /// <summary>
+        /// 列举满足商品数量的所有货架组合
+        /// </summary>
+        public List<List<int>> CombinationAllEnum(int count, Dictionary<int, int> shelf)
         {
-
+            IOrderedEnumerable<KeyValuePair<int, int>> orderShelf =  shelf.OrderBy(s => s.Value);
+            //orderShelf[2]
         }
+
+        /// <summary>
+        /// 获取一个数中所有1的位置
+        /// N位数的所有组合，视为1在二进制中的位置
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public List<int> GetOnePosition(int num)
+        {
+            List<int> result = new List<int>();
+            int tmp = num;
+            for (int i = 1; tmp > 0; i++)
+            {
+                if ((tmp & 1) > 0)
+                    result.Add(i);
+                tmp >>= 1;
+            }
+
+            return result;
+        }
+
+
+
+        //private Dictionary<int, List<List<int>>> ExplainProductShelfs(Dictionary<int, Dictionary<int, int>> productShelf)
+        //{
+
+        //}
     }
 }
