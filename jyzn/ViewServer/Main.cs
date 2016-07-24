647dd4467a457cae14ff28744f24c98285961046
zac <suoxd123@126.com>
2016-07-24 18:29:41 +0800

    新增窗体

--- jyzn/ViewServer/Main.cs
+++ jyzn/ViewServer/Main.cs
@@ -11,9 +11,9 @@ using Models;
 
 namespace ViewServer
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
--- jyzn/ViewServer/Setting.cs
+++ jyzn/ViewServer/Setting.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ViewPick
+namespace ViewServer
 {
-    public partial class Form1 : Form
+    public partial class Setting : Form
     {
-        public Form1()
+        public Setting()
         {
             InitializeComponent();
         }
